49e479b2813481ac0d83eef1626350c00963e47d
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-09-30 23:13:25 +0900

    world 기능 함수  생성

--- fall_ball.py
+++ fall_ball.py
@@ -40,3 +40,49 @@ class MaxBall:
         self.x -= self.speed
         pass
 
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+    pass
+
+
+def reset_world():
+    global running, world
+    running = True
+    minnum = random.randint(0, 20)
+    world = []
+
+    maxballs = [MaxBall() for _ in range(20 - minnum)]
+    world += maxballs
+    minballs = [MinBall() for _ in range(minnum)]
+    world += minballs
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    clear_canvas()
+
+    update_canvas()
+
+
+open_canvas()
+
+reset_world()
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+close_canvas()
